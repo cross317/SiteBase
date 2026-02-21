602d9e1ca972daf0c3bcf48de2402d5c088185b1
cross317 <mario.linked.mail@gmail.com>
2026-02-21 21:44:09 +0100

    initial

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask, render_template, redirect, request, url_for, send_from_directory, after_this_request, send_file
+import os, shutil, uuid
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
+
+    if request.method == "GET":
+        return render_template("create.html")
+
+    siteParts = request.get_json()
+
+    clean_data = {}
+    for key, value in siteParts.items():
+        clean_key = key.lstrip('#').lstrip('.') 
+        clean_data[clean_key] = value
+
+    html = render_template(
+        "siteCode.html",
+        data=clean_data
+    )
+
+    filename = "siteCode2.html"
+    path = f"static/{filename}"
+
+    fold_id = uuid.uuid4()
+
+    cartella_utente = "website_temp" + str(fold_id)
+    fold_no_ext = os.path.join("downloads", cartella_utente)
+
+    os.makedirs("downloads", exist_ok=True)
+
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(html)
+
+    os.makedirs(fold_no_ext, exist_ok=True)
+
+    index = os.path.join(fold_no_ext, "index.html")
+
+    with open("static/siteCode2.html", "r", encoding="utf-8") as f_old:
+        contenuto = f_old.read()
+
+    with open(index, "w", encoding="utf-8") as f_new:
+        f_new.write(contenuto)
+
+    with open("static/css/template-one-style.css", "r", encoding="utf-8") as f_css_old:
+        css_content = f_css_old.read()
+    style = css_content.replace("../img/", "img/")
+
+    with open(os.path.join(fold_no_ext, "style.css"), "w", encoding="utf-8") as f_css_new:
+        f_css_new.write(style)
+
+    with open("static/template-one-script.js", "r", encoding="utf-8") as f_js_old:
+        js_content = f_js_old.read()
+    with open(os.path.join(fold_no_ext, "script.js"), "w", encoding="utf-8") as f_js_new:
+        f_js_new.write(js_content)
+
+    imgs_sorgente = "static/img"
+
+    img_destinazione = os.path.join(fold_no_ext, "img")
+
+    shutil.copytree(imgs_sorgente, img_destinazione, dirs_exist_ok=True)
+
+    shutil.make_archive(fold_no_ext, 'zip', fold_no_ext)
+    
+    shutil.rmtree(fold_no_ext)
+    
+
+    return {
+        "success": True,
+    }
+
+@app.route('/template-one-index')
+def anteprima():
+    return render_template("user-templates/template-one/template-one-index.html")
+
+
+@app.route("/downloads")
+def download_file():
+    global cartella_utente
+    """fPath = os.path.join("downloads", "website_temp.zip")
+    @after_this_request
+    def rem_zip(response):
+        try:
+            os.remove(fPath)
+        except Exception as error:
+            print(f"Not able tu run code because of: {error}")
+        return response"""
+
+    return send_from_directory(
+        directory="downloads",
+        path=cartella_utente,
+        as_attachment=True
+    )
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,89 @@
+if (window.location.pathname.endsWith("index.html") || window.location.pathname == ("/")) {
+    const track = document.querySelector(".track-carousel")
+    const slides = document.querySelectorAll(".slide")
+
+    const nextBtn = document.querySelector(".arrow.right");
+    const prevBtn = document.querySelector(".arrow.left");
+
+    let currentIndex = 0;
+
+    const slideWidth = slides[0].offsetWidth;
+
+    function UpdateCarousel(){  
+        const width = slides[0].getBoundingClientRect().width; 
+
+        track.style.transform = `translateX(-${currentIndex * width}px)`;
+    }
+
+    let autoSLide = setInterval(MoveNext, 3100)
+
+    function MoveNext() {
+        currentIndex++;
+        if (currentIndex > slides.length){
+            currentIndex = 0;
+        }
+        UpdateCarousel();
+    }
+
+    function ResetTimer() {
+        clearInterval(autoSLide);
+        autoSLide = setInterval(MoveNext, 3100)
+    }
+
+    nextBtn.addEventListener("click", () => {
+        currentIndex++;
+        if (currentIndex >= slides.length) {
+            currentIndex = 0;
+        }
+        UpdateCarousel();
+    })
+
+
+    prevBtn.addEventListener("click", () => {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = slides.length - 1;
+        }
+        UpdateCarousel();
+    })
+}
+
+
+let cartella_utente = ""
+
+document.addEventListener("DOMContentLoaded", () => {
+    let formTool = document.getElementById("form-tool");
+    let userInputs = document.querySelectorAll(".input-text");
+    let dl_btn = document.querySelector(".download_btn")
+
+    /** @type {HTMLIFrameElement} **/
+    const iframe = document.getElementById("previewed-frame");
+
+    iframe.addEventListener("load", () => {
+        const iDoc = iframe.contentDocument;
+        formTool.addEventListener("submit", (e) =>  {
+            e.preventDefault();
+
+            let dataPack = {}
+
+            userInputs.forEach(input => {
+                const selector = input.getAttribute("data-target");
+                const element = iDoc.querySelector(selector);
+
+                if (element) {
+                    element.textContent = input.value;
+                    dataPack[selector] = input.value;
+                }
+            });
+            alert(JSON.stringify(dataPack))
+            
+            fetch("/create", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(dataPack)
+            });
+        });
+    });
+});
