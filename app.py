2665e50c10b9313671b96346be4d2a9137626a87
cross317 <mario.linked.mail@gmail.com>
2026-02-27 00:39:16 +0100

    kk

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from flask import Flask, render_template, redirect, request, url_for, send_from_directory, after_this_request, send_file, session
-import os, shutil, uuid
+import os, shutil, uuid, time, threading
 
 app = Flask(__name__)
 
 app.secret_key = "KAWDIOSJNWDAISDJWNALOSKW"
 
+def pulisciFile():
+    fold = "downloads"
+    t_limit = 5
+
+    time.sleep(2)
+
+    while True:
+        t_now = time.time()
+
+        for file_name in os.listdir(fold):
+            extended_path = os.path.join(fold, file_name)
+            life_time = t_now - os.path.getmtime(extended_path)
+            print(life_time)
+            if life_time >= t_limit:
+                try:
+                    os.remove(extended_path)
+                except Exception as e:
+                    pass
+
+        time.sleep(5)
+
+
+thread_pulizia = threading.Thread(target=pulisciFile, daemon=True)
+thread_pulizia.start()
 
 @app.route("/")
 def index():
@@ -15,6 +39,7 @@ def index():
 def create():
 
     if request.method == "GET":
+        print("DEBUG: Sono dentro la rotta /create!") 
         return render_template("create.html")
 
     siteParts = request.get_json()
@@ -29,9 +54,6 @@ def create():
         data=clean_data
     )
 
-    filename = "siteCode2.html"
-    path = f"static/{filename}"
-
     fold_id = uuid.uuid4()
 
     cartella_utente = "website_temp" + str(fold_id)
@@ -39,18 +61,12 @@ def create():
 
     os.makedirs("downloads", exist_ok=True)
 
-    with open(path, "w", encoding="utf-8") as f:
-        f.write(html)
-
     os.makedirs(fold_no_ext, exist_ok=True)
 
     index = os.path.join(fold_no_ext, "index.html")
 
-    with open("static/siteCode2.html", "r", encoding="utf-8") as f_old:
-        contenuto = f_old.read()
-
     with open(index, "w", encoding="utf-8") as f_new:
-        f_new.write(contenuto)
+        f_new.write(html)
 
     with open("static/css/template-one-style.css", "r", encoding="utf-8") as f_css_old:
         css_content = f_css_old.read()
@@ -87,18 +103,18 @@ def anteprima():
     return render_template("user-templates/template-one/template-one-index.html")
 
 
-@app.route("/downloads")
+"""@app.route("/downloads")
 def download_file():
-    """fPath = os.path.join("downloads", "website_temp.zip")
+    fPath = os.path.join("downloads", "website_temp.zip")
     @after_this_request
     def rem_zip(response):
         try:
             os.remove(fPath)
         except Exception as error:
             print(f"Not able tu run code because of: {error}")
-        return response"""
+        return response
     
-    cartella_utente = session.get("cartella_utente")
+    cartella_utente = session.pop("cartella_utente", None)
 
     if cartella_utente:
         return send_from_directory(
@@ -109,7 +125,60 @@ def download_file():
     else:
         return "Nessun file trovato", 404
     
+    zip_path = os.path.join("downloads", cartella_utente)
+
+    @after_this_request
+    def remove_zip(response):
+        try:
+            if os.path.exists(zip_path):
+                os.remove(zip_path)
+        except Exception as e:
+            print("Errore eliminazione zip:", e)
+        return response
+
+        return send_from_directory(
+            directory="downloads",
+            path=cartella_utente,
+            as_attachment=True)"""
+
+
+"""def delete_with_retry(path: str, delay: float = 2.0, retries: int = 30, interval: float = 1.0):
+    def job():
+        time.sleep(delay)
+        for _ in range(retries):
+            try:
+                if os.path.exists(path):
+                    os.remove(path)
+                return
+            except PermissionError:
+                time.sleep(interval)
+            except Exception as e:
+                print("Errore eliminazione zip:", e)
+                return
+    threading.Thread(target=job, daemon=True).start()"""
+
+
+@app.route("/downloads")
+def download_file():
+
+    cartella_utente = session.pop("cartella_utente", None)
+
+    if not cartella_utente:
+        return "Nessun file trovato", 404
+
+    """@after_this_request
+    def unpackDelete():
+        cartella_unzipped = shutil.unpack_archive(cartella_utente)
+        normal_path = os.path.join("downloads", cartella_utente)
+
+        delete_with_retry(normal_path, delay=5, retries=60, interval=0.5)"""
+
+    return send_from_directory(
+        directory="downloads",
+        path=cartella_utente,
+        as_attachment=True
+    )
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
